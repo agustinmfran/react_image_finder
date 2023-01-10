4371a784c997dc62418133075d1275adda971918
agustinmfran <agustinmfran@gmail.com>
2023-01-10 18:54:31 +0100

    hecho el fectch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import {Formik, Form, Field} from 'formik'
+import './header.css'
 
 function App() {
+  const [photos, setPhotos] = useState([])
+  console.log({photos})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{search:''}}
+          onSubmit={async values =>{
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID 7ZwL7OuvIjN0Wc4n7d5DELbkLFF2ys3rXeiBqJVuDFo'
+              }
+            })
+            const data = await response.json()
+            setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name='search'/>
+          </Form>
+        </Formik>
       </header>
     </div>
   );
