29ceb446a0e1a5addd3611eb9173896fce144412
agustinmfran <agustinmfran@gmail.com>
2023-01-13 11:34:22 +0100

    terminado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import {useState} from 'react'
 import {Formik, Form, Field} from 'formik'
 import './header.css'
+import './content.css'
+import './article.css'
 
 function App() {
   const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
   console.log({photos})
   return (
     <div>
@@ -21,10 +24,20 @@ function App() {
           }}
         >
           <Form>
-            <Field name='search'/>
+            <label className='headerLabel'>Buscador de imagenes: </label>
+            <Field name='search' placeholder='Search'/>
           </Form>
         </Formik>
       </header>
+      <div className='container'>
+        <div className='center'>
+          {photos.map(photo =>
+            <article key={photo.id} onClick={()=> open(photo.links.html)}>
+              <img src={photo.urls.regular} alt='img'/>
+              <p>{(!photo.description ? '' : photo.description + ' - ') + photo.alt_description}</p>
+            </article>)}
+        </div>
+      </div>
     </div>
   );
 }
